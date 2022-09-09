bcc0efc0911b8009f6f1e5c23f74b32013cd8483
seonwoo0808 <seonwoo0808@naver.con>
2022-09-10 01:19:55 +0900

    fix request

--- module/requesting.py
+++ module/requesting.py
@@ -4,13 +4,16 @@ SN: str = os.environ['SN']
 backend_url: str = os.environ['BACKEND_URL']
 
 def request_stt(base64_encoded) -> str:
+    headers = {
+        'Content-Type': 'application/json; charset=utf-8',
+        'SN': SN
+        }
     param_dict: dict = {
-        'SN': SN,
-        'content': base64_encoded
+        "content": base64_encoded
     }
     param_json = json.dumps(param_dict)
     print(param_json)
-    response: requests.Response = requests.post(backend_url, json=param_json)
+    response: requests.Response = requests.post(backend_url, headers=headers,json=param_json)
     print(response.text)
     transcript: str = response.json()['transcript']
 

7711ee0b3751411e376a9cd418ae56727c40ecb6
PENTAGON <seonwoo0808@naver.com>
2022-09-17 11:54:24 +0900

    fix usb addr

--- module/board_controll.py
+++ module/board_controll.py
@@ -6,16 +6,7 @@ import time
 
 py_serial = serial.Serial(
     
-    port='COM3',
+    port='/dev/ttyUSB0',
     
     baudrate=9600,
 )
-
-while True:
-      
-    commend = input('Order: ')
-    py_serial.write(commend.encode())
-    time.sleep(0.1)
-    if py_serial.readable():
-        response = py_serial.readline()
-        print(response[:len(response)-1].decode())

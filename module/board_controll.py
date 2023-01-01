b788676d7c44e30b03e551f1714cc348220b2d42
PENTAGON <59224587+seonwoo0808@users.noreply.github.com>
2023-01-01 13:29:20 +0900

    Add Comment on Board Control Module

--- module/board_controll.py
+++ module/board_controll.py
@@ -1,5 +1,6 @@
 import serial,time
 
+# Start Connection With Arduino
 py_serial = serial.Serial(
     
     port='/dev/ttyUSB0',
@@ -7,17 +8,17 @@ py_serial = serial.Serial(
     baudrate=9600,
 )
 
-
 def change_led_color(hex_code):
     print(hex_code)
-    plain = "X" + hex_code
+    plain = "X" + hex_code # Our Protocol -> X000000
     py_serial.write(plain.encode())
 
 def change_led_bright(amount):
     print(amount)
-    plain = "B" + '{0:03d}'.format(amount)
+    plain = "B" + '{0:03d}'.format(amount) # Our Protocol -> B000
     py_serial.write(plain.encode())
 
+# Initialize LED
 change_led_color('ffffff')
 print("LED start")
 

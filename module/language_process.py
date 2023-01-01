775afd70c5cc0ca8e0be8aa89c5964ed66e09a2f
PENTAGON <59224587+seonwoo0808@users.noreply.github.com>
2023-01-01 13:38:16 +0900

    Remove Unused Function and Optimize Code

--- module/language_process.py
+++ module/language_process.py
@@ -1,12 +1,21 @@
 from multiprocessing.resource_sharer import stop
 
 
-wake_up_word_list = ['세리', '쎄리', '새리', "쌔리", '쉐리', '세이', '시리', '체리', '캐리', '혜리', '세력']
+wake_up_word_list = ['세리', '쎄리', '새리', "쌔리",]
 study_mode_word_list = ["공부모드", "공부 모드", "공부 모두"]
 
 start_word_list = ["시작", "스타트", "틀어", "켜", "재생"]
 stop_word_list = ["그만", "멈춰", "중지", "종료", "꺼"]
 
+white_noise_word_list = ["백색소음", "백색 소음"]
+volume_word_list = ["볼륨", "음량", "소리"]
+
+led_word_list = ["불", "빛", "등", "조명", "전등", "엘이디", "LED", "led"]
+
+decrease_word_list = ["낮춰", "줄여", "낮추어", "낮혀", "낮게"]
+increase_word_list = ["높여", "키워", "올려", "높혀", "높게"]
+
+change_word_list = ["바꿔", "다음", "변경", "딴", "다른"]
 
 
 def is_wake_up_word(text):
@@ -34,36 +43,37 @@ def is_stop_word(text):
     return False
 
 def is_white_noise(text):
-    if "백색소음" in text or "백색 소음" in text:
-        return True
+    for i in white_noise_word_list:
+        if i in text:
+            return True
     return False
 
 def is_volume(text):
-    if "볼륨" in text or "음량" in text or "소리" in text:
-        return True
+    for i in volume_word_list:
+        if i in text:
+            return True
     return False
 
 def is_led(text):
-    if "불" in text or "빛" in text or "등" in text or "조명" in text or "전등" in text or "전구" in text or "엘이디" in text or "LED" in text or "led" in text:
-        return True
+    for i in led_word_list:
+        if i in text:
+            return True
     return False
 
 def is_decrease_word(text):
-    if "낮춰" in text or "줄여" in text or "낮추어" in text or "낮혀" in text or "낮게" in text:
-        return True
+    for i in decrease_word_list:
+        if i in text:
+            return True
     return False
 
 def is_increase_word(text):
-    if "높여" in text or "키워" in text or "올려" in text or "높혀" in text or "높게" in text:
-        return True
+    for i in increase_word_list:
+        if i in text:
+            return True
     return False
 
 def is_change_word(text):
-    if "바꿔" in text or "다음" in text or "변경" in text or "딴" in text or "다른" in text:
-        return True
-    return False
-
-def is_study_time_info(text):
-    if "알려" in text or "공부 시간" in text or "공부시간" in text:
-        return True
+    for i in change_word_list:
+        if i in text:
+            return True
     return False
